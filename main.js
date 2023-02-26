8cb61dab55e5f3e237f3b15bcb9496d08aae302e
JaloliddinIkrom <jaloliddinikromovvv@gmail.com>
2023-02-27 01:18:45 +0500

    Add heading

--- main.js
+++ main.js
@@ -7,14 +7,17 @@
  const elInpWeightMax = document.querySelector(".js-poke-weight-max"); 
  const elSelectWeight = document.querySelector(".js-selct-weight");
  const elList = document.querySelector(".js-list");
- const sortName = document.querySelector(".js-name-sort"); 
- const elTemple = document.querySelector(".js-templet").content;
- const elFragment = document.createDocumentFragment();
+ const sortName = document.querySelector(".js-name-sort");  
+ const bookMarkList = document.querySelector(".bookmarl-list");
+ 
    
 
-  //--------Pocemonsni aylanamiz weaknesses nomli arryni ham aylanib bo'sh arryga push qilib olamiz-// 
+  //--------Pocemonsni aylanamiz weaknesses nomli arryni ham aylanib bo'sh arryga push qilib olamiz-----------// 
 
   const arryWeaknesses = []; 
+  
+  //---------------Bookmark arry---------------
+  const bookArry = [];
 
    function renderWeak(){ 
 
@@ -33,7 +36,7 @@
        
        arryWeaknesses.sort() 
    };
-//------Arryga yig'lgan elementlarni selktga joylashtrdik-------- 
+//------Arryga yig'lgan elementlarni selktga joylashtrdik------------------------------------------------/////
 
    function renderArryWeaknesses(){ 
      
@@ -50,122 +53,65 @@
 
     elSelcetWeak.appendChild(weakFragment);
    };
-// ------------------------------------------- 
-
-
-  //--------------------------------render pocemons start---------------
-   const renderPoce = function(poke){ 
-
-     elList.innerHTML = "";
-
-      poke.forEach(item =>{
-     
-  let newItem = elTemple.cloneNode(true);
-  
-  newItem.querySelector(".item-title").textContent = item.name;
-  newItem.querySelector(".itim-nambr").textContent = item.id;
-  newItem.querySelector(".item-img").src = item.img;
-  newItem.querySelector(".item-img").alt = item.name;
-  newItem.querySelector(".item-text").textContent = item.weight;
-  newItem.querySelector(".item-candy_count").textContent = item.candy_count;
-  newItem.querySelector(".item-weaknesses").textContent = item.weaknesses.join(", ");
-
- elFragment.appendChild(newItem);
- 
-         });
-
-       elList.appendChild(elFragment); 
-   };
-  
-  // ------------------------------Forim addEventListener start-----------
-
- elForm.addEventListener("submit", function(evt){ 
-  
-  evt.preventDefault();
-
-   //-----------------------------------sort wight------------------ 
-
-
-   const arryWeight = []; 
-
-     if(elSelectWeight.value == "fat-thin"){ 
-
-
-    const fat = pokemons.sort((d, c) =>{ 
- 
-        const eld = d.weight.toUpperCase();
-        const elc = c.weight.toUpperCase();
-
-       if(eld > elc){ 
-
-        return 1;
-       }else if(eld < elc){ 
-
-        return -1;
-       }else{ 
-
-        return 0;
-       }
-
-      });
-
-      arryWeight .push(fat);
-
-
-     } 
-
-    if(elSelectWeight.value == "thin-fat"){ 
-
-    const thin = pokemons.sort((t, g) => { 
-
-      const elt = t.weight.toUpperCase();
-      const elg = g.weight.toUpperCase();
-
-      if(elt < elg){ 
-
-        return 1;
-      }else if(elt > elg){ 
-
-        return -1;
-      }else{ 
-
-        return 0;
-      }
-
-    });
-
-    arryWeight.push(thin);
-    };
-
+// -----------------------------------------------------------------------------------------------------------// 
    
-    //-------------------------------------filter name stert--------------------
-     
-    const inpNameValue = elInp.value.trim();
+    function sortPocemon(arry, sortPoce){ 
 
-    const regexName = new RegExp(inpNameValue, "gi");
- 
-       const nameFiltir = pokemons.filter(item => { 
- 
-           const nameRegex =  String(item.name).match(regexName) && (elSelcetWeak.value == "all" || item.weaknesses.includes(elSelcetWeak.value)) && (elInpWeightMin.value == "" || item.candy_count >= elInpWeightMin.value) &&  (elInpWeightMax.value == "" || item.candy_count <= elInpWeightMax.value);
-             
-         
-           return nameRegex;
-       }); 
- 
-       if(nameFiltir.length > 0){ 
- 
-         renderPoce(nameFiltir)
-       }else{ 
- 
-         elList.innerHTML = "Movie not fount"
-       };
-    
-  //----------------------------------------sort name start----------------------------
-     const sortNameArry = [];
-     
-     if(sortName.value == "Aa-Zz"){ 
+
+    //-----------------------------------sort wight-----------------------------------------------------------/// 
+
+        if(sortPoce == "fat-thin"){ 
+
+
+        arry.sort((d, c) =>{ 
+
+      const eld = d.weight.toUpperCase();
+      const elc = c.weight.toUpperCase();
+
+     if(eld > elc){ 
+
+      return 1;
+     }else if(eld < elc){ 
+
+      return -1;
+     }else{ 
+
+      return 0;
+     }
+
+           });
+
+
+        } 
+
+        if(sortPoce == "thin-fat"){ 
+
+       arry.sort((t, g) => { 
+
+    const elt = t.weight.toUpperCase();
+    const elg = g.weight.toUpperCase();
+
+    if(elt < elg){ 
+
+      return 1;
+    }else if(elt > elg){ 
+
+      return -1;
+    }else{ 
+
+      return 0;
+    }
+
+  });
+
+         };
+       
+  //----------------------------------------sort name start----------------------------------------------------//
+
+          
+         if(sortPoce == "Aa-Zz"){ 
       
-      const aName = pokemons.sort((a, b) =>{ 
+          arry.sort((a, b) =>{ 
      
        const nameA = a.name.toUpperCase();
        const nameB = b.name.toUpperCase();
@@ -183,12 +129,10 @@
 
       });
 
-      sortNameArry.push(aName);
+         }  
+         if(sortPoce == "Zz-Aa"){ 
 
-     }  
-     if(sortName.value == "Zz-Aa"){ 
-
-        const zName = pokemons.sort((x, l) =>{ 
+        arry.sort((x, l) =>{ 
 
           const nameX = x.name.toUpperCase();
           const nameL = l.name.toUpperCase();
@@ -205,20 +149,157 @@
           }
 
         });
-        sortNameArry.push(zName);
-     };
+     
+         };
+    };
+
+  //--------------------------------render pocemons start-----------------------------------------------------//
+   const renderPoce = function(poke, regex = ""){  
+
+    const elTemple = document.querySelector(".js-templet").content;
+    const elFragment = document.createDocumentFragment();
+
+     elList.innerHTML = "";
+
+      poke.forEach(item =>{
+     
+        let newItem = elTemple.cloneNode(true); 
+
+       if(regex.source != "(?:)" && regex){ 
+
+    newItem.querySelector(".item-title").innerHTML = item.name.replace(regex, `<mark class"">${ 
+           
+      regex.source.toLowerCase()
+
+    }</mark>`);  
+
+       }else{ 
+
+    newItem.querySelector(".item-title").textContent = item.name; 
+  }
+  
+  newItem.querySelector(".itim-nambr").textContent = item.id;
+  newItem.querySelector(".item-img").src = item.img;
+  newItem.querySelector(".item-img").alt = item.name;
+  newItem.querySelector(".item-text").textContent = item.weight;
+  newItem.querySelector(".item-candy_count").textContent = item.candy_count;
+  newItem.querySelector(".item-weaknesses").textContent = item.weaknesses.join(", ");
+  newItem.querySelector(".item-star").dataset.bookmarkId = item.name; 
+
+  elFragment.appendChild(newItem);
+ 
+      });
+
+      elList.appendChild(elFragment); 
+   };  
+
+
+  //-------------------------------- reder addBokmark star--------------------------------------------------// 
+
+   const renderBokmark = function(bookmark, node){  
+
+    const elTemple = document.querySelector(".js-templet").content;
+    const elFragment = document.createDocumentFragment();
+
+     bookMarkList.innerHTML = "";
+
+      bookmark.forEach(item =>{
+     
+        let newItem = elTemple.cloneNode(true); 
+
+       
+  newItem.querySelector(".item-title").textContent = item.name; 
+  newItem.querySelector(".itim-nambr").textContent = item.id;
+  newItem.querySelector(".item-img").src = item.img;
+  newItem.querySelector(".item-img").alt = item.name;
+  newItem.querySelector(".item-text").textContent = item.weight;
+  newItem.querySelector(".item-candy_count").textContent = item.candy_count;
+  newItem.querySelector(".item-weaknesses").textContent = item.weaknesses.join(", ");    
+  newItem.querySelector(".temp-butten-dalet").dataset.bookmarkId = item.name; 
+  newItem.querySelector(".item-star").classList.add("visually-hidden");    
+  newItem.querySelector(".temp-butten-dalet").classList.remove("visually-hidden");   
+
+  elFragment.appendChild(newItem);
+ 
+      });
+
+      node.appendChild(elFragment); 
+   }; 
+
+                                        // Event Delegation
+
+  //--------------------------------elList addEventListener star---------------------------------------------//
+
+    elList.addEventListener("click", function(evt){ 
+
+         if(evt.target.matches(".item-star")){ 
+
+            const bookId = evt.target.dataset.bookmarkId;
+            
+            const addBookmark = pokemons.find(item => item.name === bookId);
+
+              if(!bookArry.includes(addBookmark)){ 
+
+                 
+                   bookArry.push(addBookmark);
+
+                   renderBokmark(bookArry, bookMarkList);
+              }
+             
+         } 
+         
+
+    }); 
+    //-----------------------------------bookmar list addEventListener-------------------------------------// 
+    bookMarkList.addEventListener("click", function(evt){  
+
+      if(evt.target.matches(".temp-butten-dalet")){ 
+        
+        const evtBookmark = evt.target.dataset.bookmarkId;
+
+        const daletBukmark = pokemons.find(item => item.name == evtBookmark);
+
+        bookArry.splice(daletBukmark, 1); 
+
+        renderBokmark(bookArry, bookMarkList); 
+
+      }
+    })
+    
+  // ------------------------------Forim addEventListener start-------------------------------------------------//
+
+    elForm.addEventListener("submit", function(evt){ 
+  
+  evt.preventDefault();
+
+    //-------------------------------------filter name stert--------------------
+     
+    const inpNameValue = elInp.value.trim();
+
+    const regexName = new RegExp(inpNameValue, "gi");
+ 
+       const nameFiltir = pokemons.filter(item => { 
+ 
+           const nameRegex =  String(item.name).match(regexName) && (elSelcetWeak.value == "all" || item.weaknesses.includes(elSelcetWeak.value)) && (elInpWeightMin.value == "" || item.candy_count >= elInpWeightMin.value) &&  (elInpWeightMax.value == "" || item.candy_count <= elInpWeightMax.value);
+             
+         
+           return nameRegex;
+       }); 
+ 
+       if(nameFiltir.length > 0){ 
+         
+        sortPocemon(nameFiltir, sortName.value);
+         renderPoce(nameFiltir, regexName);
+       
+       }else{ 
+ 
+         elList.innerHTML = "Movie not fount"
+       };
     
     }); 
 
         
-        renderWeak() 
-        renderArryWeaknesses()
+        renderWeak(); 
+        renderArryWeaknesses();
         renderPoce(pokemons); 
-
-
-        // 1. name property bo'yicha search qilish. 
-        // 2. weaknesses arrayini ichidagi filter.
-        // 3. candy_count orqali filter qilish. min to max
-        // 4. name propertysi orqali sort qilish A-Z , Z-A va weight property orqali sort qilish ya'ni ozg'indan semizga , semizdan ozg'inga.
-        // 5. Style muhim, responsive bo'lishi ham shart. 
-        // 6. Bookmark function bo'lishi shart.
+        
